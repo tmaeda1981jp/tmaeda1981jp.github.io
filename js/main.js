a173658ff077247c38d8e7ee07687acd1079ecb2
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-10-15 01:56:31 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*jslint white: true, nomen: true, maxlen: 120, plusplus: true, */
+/*global _:false, $:false, define:false, require:false, */
+
+$(function() {
+
+  $(document).one('click', '#logo', function() {
+    $(this).css({
+      transform: 'rotate(360deg)'
+    });
+  });
+
+});
